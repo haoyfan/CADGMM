12f2226cee8fe583915c7ebe075bac2d207616f4
Haoyi Fan <isfanhy@gmail.com>
2021-01-02 21:49:35 +0800

    update

--- data/kdd.py
+++ data/kdd.py
@@ -14,9 +14,9 @@ def get_test(label=0,v=0,scale=False, *args):
     """Get testing dataset for KDD 10 percent"""
     return _get_adapted_dataset("test", scale,v)
 
-def get_valid(label=0, scale=False,v=0, *args):
-    """Get validation dataset for Thyroid dataset"""
-    return _get_adapted_dataset("valid", scale,v)
+def get_valid(label=0, scale=False, *args):
+    """Get validation dataset for KDD 10 percent"""
+    return None
 
 def get_shape_input():
     """Get shape of the dataset for KDD 10 percent"""
@@ -53,64 +53,113 @@ def _get_dataset(scale,v):
     labels[labels == 'normal.'] = 1
 
     df['label'] = labels
-    dataset = df.sample(frac=1, random_state=42)
-    features, labels = _to_xy(dataset, target='label')
 
-    size_alldata = np.size(features,axis=0)
+    df_train = df.sample(frac=0.5, random_state=42)
+    df_test = df.loc[~df.index.isin(df_train.index)]
 
-    normal_x_data = features[labels == 0]
-    normal_y_data = labels[labels == 0]
-    size_nomaldata = normal_x_data.shape[0]
+    x_train, y_train = _to_xy(df_train, target='label')
+    x_test, y_test = _to_xy(df_test, target='label')
 
-    size_traindata = size_nomaldata
+    y_train = y_train.flatten().astype(int)
+    y_test = y_test.flatten().astype(int)
+    x_train = x_train[y_train != 1]
+    y_train = y_train[y_train != 1]
+    if scale:
+        print("Scaling KDD dataset")
+        scaler = MinMaxScaler()
+        scaler.fit(x_train)
+        x_train = scaler.transform(x_train)
+        x_test = scaler.transform(x_test)
 
-    anormal_x_data = features[labels == 1]
-    anormal_y_data = labels[labels == 1]
-    size_anormaldata = anormal_x_data.shape[0]
-    size_tndata = size_traindata - size_anormaldata//2
+    dataset = {}
+    dataset['x_train'] = x_train.astype(np.float32)
+    dataset['y_train'] = y_train.astype(np.float32)
+    dataset['x_test'] = x_test.astype(np.float32)
+    dataset['y_test'] = y_test.astype(np.float32)
 
-    randNdata = np.arange(size_nomaldata)
-    randAdata = np.arange(size_anormaldata)
-    np.random.shuffle(randNdata)
-    np.random.shuffle(randAdata)
-    x_tndata = normal_x_data[randNdata[:size_tndata]]
-    y_tndata = normal_y_data[randNdata[:size_tndata]]
-    x_tadata = anormal_x_data[randAdata[:size_anormaldata//2]]
-    y_tadata = anormal_y_data[randAdata[:size_anormaldata//2]]
+    return dataset
+def _get_dataset(scale,v):
+    """ Gets the basic dataset
+    Returns :
+            dataset (dict): containing the data
+                dataset['x_train'] (np.array): training images shape
+                (?, 120)
+                dataset['y_train'] (np.array): training labels shape
+                (?,)
+                dataset['x_test'] (np.array): testing images shape
+                (?, 120)
+                dataset['y_test'] (np.array): testing labels shape
+                (?,)
+    """
+    col_names = _col_names()
+    df = pd.read_csv("data/kddcup.data_10_percent_corrected", header=None, names=col_names)
+    text_l = ['protocol_type', 'service', 'flag', 'land', 'logged_in', 'is_host_login', 'is_guest_login']
 
-    x_vndata = normal_x_data[randNdata[:size_tndata//2]]
-    y_vndata = normal_y_data[randNdata[:size_tndata//2]]
-    x_vadata = anormal_x_data[randAdata[:size_anormaldata//2]]
-    y_vadata = anormal_y_data[randAdata[:size_anormaldata//2]]
+    for name in text_l:
+        _encode_text_dummy(df, name)
 
+    labels = df['label'].copy()
+    labels[labels != 'normal.'] = 0
+    labels[labels == 'normal.'] = 1
 
-    x_tendata = normal_x_data[randNdata[size_tndata//2:size_tndata]]
-    y_tendata = normal_y_data[randNdata[size_tndata//2:size_tndata]]
-    x_teadata = anormal_x_data[randAdata[size_anormaldata//2:]]
-    y_teadata = anormal_y_data[randAdata[size_anormaldata//2:]]
-    x_train = np.concatenate((x_tndata, x_tadata), axis=0)
-    y_train = np.concatenate((y_tndata, y_tadata), axis=0)
-    N_train = x_train.shape[0]
-    randIdt = np.arange(N_train)
-    np.random.shuffle(randIdt)
-    x_train = x_train[randIdt[:]]
-    y_train = y_train[randIdt[:]]
+    df['label'] = labels
+    if v == 0:
+        df_train = df.sample(frac=0.5, random_state=42)
+        df_test = df.loc[~df.index.isin(df_train.index)]
 
-    x_valid = np.concatenate((x_vndata, x_vadata), axis=0)
-    y_valid = np.concatenate((y_vndata, y_vadata), axis=0)
-    N_valid = x_valid.shape[0]
-    randIdt = np.arange(N_valid)
-    np.random.shuffle(randIdt)
-    x_valid = x_valid[randIdt[:]]
-    y_valid = y_valid[randIdt[:]]
+        x_train, y_train = _to_xy(df_train, target='label')
+        x_test, y_test = _to_xy(df_test, target='label')
 
-    x_test = np.concatenate((x_tendata, x_teadata), axis=0)
-    y_test = np.concatenate((y_tendata, y_teadata), axis=0)
-    N_test = x_test.shape[0]
-    randIdt = np.arange(N_test)
-    np.random.shuffle(randIdt)
-    x_test = x_test[randIdt[:]]
-    y_test = y_test[randIdt[:]]
+        y_train = y_train.flatten().astype(int)
+        y_test = y_test.flatten().astype(int)
+        x_train = x_train[y_train != 1]
+        y_train = y_train[y_train != 1]
+    else:
+        dataset = df.sample(frac=1, random_state=42)
+        features, labels = _to_xy(dataset, target='label')
+
+        size_alldata = np.size(features,axis=0)
+
+        size_traindata = size_alldata//2
+
+        normal_x_data = features[labels == 0]
+        normal_y_data = labels[labels == 0]
+        size_nomaldata = normal_x_data.shape[0]
+
+        anormal_x_data = features[labels == 1]
+        anormal_y_data = labels[labels == 1]
+        size_anormaldata = anormal_x_data.shape[0]
+        size_tadata = int(v * size_anormaldata)
+        size_tndata = size_traindata + size_tadata
+
+        randNdata = np.arange(size_nomaldata)
+        randAdata = np.arange(size_anormaldata)
+        np.random.shuffle(randNdata)
+        np.random.shuffle(randAdata)
+        x_tndata = normal_x_data[randNdata[:size_tndata]]
+        y_tndata = normal_y_data[randNdata[:size_tndata]]
+        x_tadata = anormal_x_data[randAdata[:size_tadata]]
+        y_tadata = anormal_y_data[randAdata[:size_tadata]]
+
+        x_tendata = normal_x_data[randNdata[size_tndata:]]
+        y_tendata = normal_y_data[randNdata[size_tndata:]]
+        x_teadata = anormal_x_data[randAdata[size_tadata:]]
+        y_teadata = anormal_y_data[randAdata[size_tadata:]]
+        x_train = np.concatenate((x_tndata, x_tadata), axis=0)
+        y_train = np.concatenate((y_tndata, y_tadata), axis=0)
+        N_train = x_train.shape[0]
+        randIdt = np.arange(N_train)
+        np.random.shuffle(randIdt)
+        x_train = x_train[randIdt[:]]
+        y_train = y_train[randIdt[:]]
+
+        x_test = np.concatenate((x_tendata, x_teadata), axis=0)
+        y_test = np.concatenate((y_tendata, y_teadata), axis=0)
+        N_test = x_test.shape[0]
+        randIdt = np.arange(N_test)
+        np.random.shuffle(randIdt)
+        x_test = x_test[randIdt[:]]
+        y_test = y_test[randIdt[:]]
     if scale:
         print("Scaling KDD dataset")
         scaler = MinMaxScaler()
@@ -122,8 +171,6 @@ def _get_dataset(scale,v):
     dataset = {}
     dataset['x_train'] = x_train.astype(np.float32)
     dataset['y_train'] = y_train.astype(np.float32)
-    dataset['x_valid'] = x_test.astype(np.float32)
-    dataset['y_valid'] = y_test.astype(np.float32)
     dataset['x_test'] = x_test.astype(np.float32)
     dataset['y_test'] = y_test.astype(np.float32)
     normal_ratio_train = int(len(np.where(y_train == 0)[0]) / len(y_train) * 100)
@@ -170,7 +217,6 @@ def _to_xy(df, target):
             result.append(x)
     dummies = df[target]
     return df.as_matrix(result).astype(np.float32), dummies.as_matrix().flatten().astype(int)
-    # return df.values.astype(np.float32), dummies.values.flatten().astype(int)
 
 def _col_names():
     """Column names of the dataframe"""
